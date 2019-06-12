107a96cb2b1a6411de0bcf3f8dcceeaefa0168bd
Diego Sandoval <sandov@protonmail.com>
2019-06-12 16:04:37 -0400

    Crea componentes para semestres, ramos y añade estilos

--- src/index.js
+++ src/index.js
@@ -2,22 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let ramos = {
+    1: ["Álgebra", "Geometría", "Carpintería nuclear"],
+    2: ["Cálculo en una variable", "Física: Mecánica"]
+}
+
 class Semestre extends React.Component {
     render(){
         return (
-            <div classname="semestre">
-                {"contenido_semestre"}
+            <div className="semestre">
+                <h2>Semestre {this.props.num}</h2>
+                <Asignatura />
             </div>
         );
     }
 }
 
+class Asignatura extends React.Component {
+    render(){
+        return (
+            <div className="asignatura">
+                {this.props.nombre}
+            </div>
+        );
+    }
+}   
+
 class App extends React.Component {
     render() {
         return(
-            <div classname="semestres_container">
-                <Semestre />
-                <Semestre />
+            <div className="app">
+                <Semestre num={1} />
+                <Semestre num={2} />
+                <Semestre num={3} />
+                <Semestre num={4} />
             </div>
         );
     }
@@ -27,3 +45,4 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
