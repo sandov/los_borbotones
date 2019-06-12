71b446b41af624ebbaf318528d2b44b37b77dcdc
Diego Sandoval <sandov@protonmail.com>
2019-06-12 16:50:10 -0400

    Agrega ramos a los semestres en los componentes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let ramos = {
-    1: ["Álgebra", "Geometría", "Carpintería nuclear"],
-    2: ["Cálculo en una variable", "Física: Mecánica"]
-}
+let ramos = [
+    ["Álgebra para Ingeniería", "Geometría para Ingeniería", "Introducción a la Ingeniería", "Introducción a la Programación"], 
+    ["Cálculo I para Ingeniería", "Física I para Ingeniería", "Álgebra Lineal para Ingeniería", "Programación"],
+    ["Física II para Ingeniería", "Ecuaciones Diferenciales para Ingeniería", "Programación Orientada a Objetos", "Estructuras discretas"]
+];
 
 class Semestre extends React.Component {
+  
     render(){
+        let numero_semestre = 1;
+        
+        if (this.props.num == 2)
+            numero_semestre  = 2;
+        if (this.props.num == 3)
+            numero_semestre  = 3;
+
         return (
             <div className="semestre">
                 <h2>Semestre {this.props.num}</h2>
-                <Asignatura />
+                <Asignatura nombre={ramos[numero_semestre-1][0]} />
+                <Asignatura nombre={ramos[numero_semestre-1][1]} />
+                <Asignatura nombre={ramos[numero_semestre-1][2]} />
+                <Asignatura nombre={ramos[numero_semestre-1][3]} />
             </div>
         );
     }
@@ -35,7 +47,6 @@ class App extends React.Component {
                 <Semestre num={1} />
                 <Semestre num={2} />
                 <Semestre num={3} />
-                <Semestre num={4} />
             </div>
         );
     }
