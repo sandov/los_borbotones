cd040ff3a36bdefef72c2efd5063b479de2602d8
Diego Sandoval <sandov@protonmail.com>
2019-06-25 22:37:44 -0400

    Completa malla, cambia a 2008 en h1. aclara color tooltips

--- src/res/malla.js
+++ src/res/malla.js
@@ -270,7 +270,347 @@ const malla =
                     requisitos: [4,7,8]
                 }
             ]
-    }
+    },
+    {
+        num_sem: 6,
+        ramos: 
+            [
+                {
+                    id: 30, 
+                    nombre: "Programación Concurrente",
+                    codigo: "INFO163",
+                    creditos: 4,
+                    tasa_repr: 0.17,
+                    requisitos: [23]
+                },
+                {
+                    id: 31,
+                    nombre: "Compiladores",
+                    codigo: "INFO165",
+                    creditos: 5,
+                    tasa_repr: 0.22,
+                    requisitos: [26]
+                },
+                {
+                    id: 32,
+                    nombre: "Investigación Operativa II",
+                    codigo: "INFO173",
+                    creditos: 5,
+                    tasa_repr: 0.19,
+                    requisitos: [13,19,27]
+                },
+                {
+                    id: 33, 
+                    nombre: "Analisis de Sistemas Linales",
+                    codigo: "INFO183",
+                    creditos: 5,
+                    tasa_repr: 0.27,
+                    requisitos: [15,29]
+                },
+                {
+                    id: 34,
+                    nombre: "Base de Datos",
+                    codigo: "INFO261",
+                    creditos: 6,
+                    tasa_repr: 0.13,
+                    requisitos: [26]
+                },
+                 {
+                    id: 35,
+                    nombre: "Computación Gráfica",
+                    codigo: "INFO265",
+                    creditos: 6,
+                    tasa_repr: 0.03,
+                    requisitos: [17,23]
+                },
+            ]
+    },
+    {
+        num_sem: 7,
+        ramos: 
+            [
+                {
+                    id: 36, 
+                    nombre: "Economía General",
+                    codigo: "IECO102",
+                    creditos: 4,
+                    tasa_repr: 0.12,
+                    requisitos: [13,17]
+                },
+                {
+                    id: 37,
+                    nombre: "Sistemas Operativos",
+                    codigo: "INFO182",
+                    creditos: 4,
+                    tasa_repr: 0.21,
+                    requisitos: [30]
+                },
+                {
+                    id: 38,
+                    nombre: "Comunicaciones",
+                    codigo: "INFO185",
+                    creditos: 6,
+                    tasa_repr: 0.05,
+                    requisitos: [13,19,29]
+                },
+                {
+                    id: 39, 
+                    nombre: "Software de Ingeniería",
+                    codigo: "INFO189",
+                    creditos: 4,
+                    tasa_repr: 0.12,
+                    requisitos: [14,20,22,33]
+                },
+                {
+                    id: 40,
+                    nombre: "Fundamentos de Ingeniería de Software",
+                    codigo: "INFO263",
+                    creditos: 5,
+                    tasa_repr: 0.08,
+                    requisitos: [25,28]
+                },
+                 {
+                    id: 41,
+                    nombre: "Ingeniería de Sistemas",
+                    codigo: "INFO270",
+                    creditos: 4,
+                    tasa_repr: 0.12,
+                    requisitos: [26]
+                },
+            ]
+    },
+    {
+        num_sem: 8,
+        ramos: 
+            [
+                {
+                    id: 42, 
+                    nombre: "Taller de Ingeniería de Software",
+                    codigo: "INFO264",
+                    creditos: 6,
+                    tasa_repr: 0.03,
+                    requisitos: [17,34,40]
+                },
+                {
+                    id: 43,
+                    nombre: "Interfaz Humano-Computador",
+                    codigo: "INFO266",
+                    creditos: 6,
+                    tasa_repr: 0.04,
+                    requisitos: [40]
+                },
+                {
+                    id: 44,
+                    nombre: "Sistemas de Información",
+                    codigo: "INFO271",
+                    creditos: 4,
+                    tasa_repr:0.03,
+                    requisitos: [25,36,41] 
+                },
+                {
+                    id: 45, 
+                    nombre: "Ingeniería Económica",
+                    codigo: "INFO272",
+                    creditos: 4,
+                    tasa_repr: 0.14,
+                    requisitos: [32,36]
+                },
+                {
+                    id: 46,
+                    nombre: "Redes",
+                    codigo: "INFO281",
+                    creditos: 6,
+                    tasa_repr: 0.13,
+                    requisitos: [37,38]
+                },
+                 {
+                    id: 47,
+                    nombre: "Taller de Integración de TIC",
+                    codigo: "INFO284",
+                    creditos: 6,
+                    tasa_repr: 0.03,
+                    requisitos: [21,28,33]
+                },
+                ]
+            },
+        {
+        num_sem: 9,
+        ramos: 
+            [
+                {
+                    id: 48, 
+                    nombre: "Inglés Funcional",
+                    codigo: "CIDI108",
+                    creditos: 4,
+                    tasa_repr: 0.05,
+                    requisitos: []
+                },
+                {
+                    id: 49,
+                    nombre: "Práctica Profesional",
+                    codigo: "EICI297",
+                    creditos: 8,
+                    tasa_repr: 0,
+                    requisitos: [42,43,44,45,46,47]
+                },
+                {
+                    id: 50,
+                    nombre: "Optativo Profesional I",
+                    codigo: "ELECT02",
+                    creditos: 0,
+                    tasa_repr: 0,
+                    requisitos: []
+                },
+                {
+                    id: 51, 
+                    nombre: "Seminario de Ética Profesional",
+                    codigo: "INFO251",
+                    creditos: 2,
+                    tasa_repr: 0,
+                    requisitos: [42,45,46]
+                },
+                {
+                    id: 52,
+                    nombre: "Proyecto I",
+                    codigo: "INFO252",
+                    creditos: 4,
+                    tasa_repr: 0.03,
+                    requisitos: [42,44,45]
+                },
+                 {
+                    id: 53,
+                    nombre: "Taller de Base de Datos",
+                    codigo: "INFO268",
+                    creditos: 4,
+                    tasa_repr: 0.008,
+                    requisitos: [42]
+                },
+                {
+                    id: 54,
+                    nombre: "Evaluación de Desempeño",
+                    codigo: "INFO273",
+                    creditos: 5,
+                    tasa_repr: 0.025,
+                    requisitos: [32,37,46]
+                },
+                {
+                    id: 55,
+                    nombre: "Sistemas de Gestión Estrategicos",
+                    codigo: "INFO275",
+                    creditos: 5,
+                    tasa_repr: 0.025,
+                    requisitos: [25,41,45]
+                },
+            ]
+    },
+    {
+        num_sem: 10,
+        ramos: 
+            [   
+             {
+                    id: 56, 
+                    nombre: "Taller de Software-Proyecto Integración de TIC",
+                    codigo: "EICI245",
+                    creditos: 5,
+                    tasa_repr: 0,
+                    requisitos: [39,42,43,46,47]
+                },
+                {
+                    id: 57, 
+                    nombre: "Arquitectura de Sistemas",
+                    codigo: "EICI246",
+                    creditos: 6,
+                    tasa_repr: 0.07,
+                    requisitos: [44,55,42,46,47]
+                },
+                {
+                    id: 58,
+                    nombre: "Realidad Tecnológica Nacional",
+                    codigo: "EICI247",
+                    creditos: 2,
+                    tasa_repr: 0,
+                    requisitos: [42,45,46]
+                },
+                {
+                    id: 59,
+                    nombre: "Inteligencia Artificial",
+                    codigo: "INFO267",
+                    creditos: 6,
+                    tasa_repr: 0.09,
+                    requisitos: [28]
+                },
+                {
+                    id: 60, 
+                    nombre: "Simulación",
+                    codigo: "INFO274",
+                    creditos: 4,
+                    tasa_repr: 0.04,
+                    requisitos: [32]
+                },
+                {
+                    id: 61,
+                    nombre: "Sistema de Gestión Tácticos",
+                    codigo: "INFO276",
+                    creditos: 4,
+                    tasa_repr: 0.008,
+                    requisitos: [25,41,45]
+                },
+         ]
+    },
+    {
+        num_sem: 11,
+        ramos: 
+            [
+                {
+                    id: 62, 
+                    nombre: "Administración de TIC",
+                    codigo: "EICI255",
+                    creditos: 6,
+                    tasa_repr: 0.05,
+                    requisitos: [56,57,61]
+                },
+                {
+                    id: 63,
+                    nombre: "Estrategia y Sistemas",
+                    codigo: "EICI261",
+                    creditos: 6,
+                    tasa_repr: 0,
+                    requisitos: [57,61]
+                },
+                {
+                    id: 64,
+                    nombre: "Proyecto de Titulo",
+                    codigo: "EICI298",
+                    creditos: 6,
+                    tasa_repr: 0,
+                    requisitos: []
+                },
+                {
+                    id: 65, 
+                    nombre: "Optativo Profesional II",
+                    codigo: "ELEC21",
+                    creditos: 0,
+                    tasa_repr: 0,
+                    requisitos: []
+                },
+                {
+                    id: 66,
+                    nombre: "Optativo Profesional III",
+                    codigo: "ELEC22",
+                    creditos: 0,
+                    tasa_repr: 0.13,
+                    requisitos: []
+                },
+                                {
+                    id: 67,
+                    nombre: "AnteProyecto de Titulo",
+                    codigo: "INFO253",
+                    creditos: 4,
+                    tasa_repr: 0.03,
+                    requisitos: [56,57,61]
+                },
+        ]
+    },
 ];
 
 export default malla;
