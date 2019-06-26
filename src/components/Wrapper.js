d5539fe3d4265d389abdb6ed4892f2a2dbd18001
Diego Sandoval <sandov@protonmail.com>
2019-06-26 04:08:41 -0400

    Hace funcionar el selector de mallas, rudimentariamente

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -14,28 +14,44 @@ class Wrapper extends React.Component{
         this.handleSelection = this.handleSelection.bind(this);
     }
 
-
     render(){
 
-    let malla = malla_carpinteria_99;
-
-        if (1) return(
-            <form>
-                <select>
-                    <option value="informatica">informatica_08</option>
-                    <option value="carpinteria">carpinteria</option>
-                </select> 
-                <input type="submit"></input>
-            </form>
-        )
+        let malla;
+        if (this.state.malla == "informatica_08")
+        {malla = malla_informatica_08;}
+        else if(this.state.malla == "carpinteria_99")
+        {malla = malla_carpinteria_99;}
     
-        else return(
-            <App malla={malla}/>
+        if (this.state.seleccionado)
+            return(
+                <App malla={malla}/>
         );
+        else
+            return(
+                <div className="selector_container">
+                    <h1>Seleccionar carrera y malla</h1>
+                    <form name="selector_malla">
+                        <select name="carrera">
+                            <option value="informatica_08">informatica 2008</option>
+                            <option value="carpinteria_99">carpinteria 2099</option>
+                        </select> 
+                        <br/>
+                        <button onClick={this.handleSelection}>Ver malla</button>
+                    </form>
+                </div>
+            );
     }    
 
     handleSelection(){
+        let form = document.forms[0];
+        let malla_seleccionada = form.elements.carrera.value;
 
+        this.setState(
+            {
+                seleccionado: true,
+                malla : malla_seleccionada
+            }
+        );
     }
 }
 
