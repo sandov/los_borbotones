6b858791cc89305ad693c60198d82196cddffef0
Diego Sandoval <sandov@protonmail.com>
2019-07-18 23:29:26 -0400

    Empezando archivo obras

---
+++ src/res/malla_obras_18.js
@@ -0,0 +1,17 @@
+const malla_obras_18 = 
+[
+    {
+        num_sem: 1,
+        ramos:
+            [
+                {
+                    id: 0, //debe ser único
+                    nombre: "OFG 1",
+                    codigo: "ELECT12",
+                    creditos: 0,
+                    tasa_repr: 0.298206,
+                    requisitos: []
+                }
+            ]
+    }
+];
